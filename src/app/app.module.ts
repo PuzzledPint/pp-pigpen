072de6e7851eacfbcb3273e508eb1dabd80b8dda
Neal Tibrewala <Neal@Tibrewala.net>
2019-01-27 11:15:00 -0600

    Added Firebase Dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Angular Fire Libs
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// Firebase credentials (okay to be public)
+const config = {
+  apiKey: 'AIzaSyAjhlNnJzXejhVD_sJIP7q0nMNd84y9vnM',
+  authDomain: 'pp-pigpen.firebaseapp.com',
+  databaseURL: 'https://pp-pigpen.firebaseio.com',
+  projectId: 'pp-pigpen',
+  storageBucket: 'pp-pigpen.appspot.com',
+  messagingSenderId: '692886874617'
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
+  declarations: [AppComponent],
+  imports: [BrowserModule,
+    // AngularFire
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
