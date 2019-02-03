f314da52c99c600976e33551d17b52c4a62e3e71
Neal Tibrewala <Neal@Tibrewala.net>
2019-02-03 14:29:01 -0600

    Added serviceWorker schematic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { FooterComponent } from '../components/footer.component';
 import { HeaderComponent } from '../components/header.component';
 import { VariablesPipe } from '../pipes/variables.pipe';
+import { TestAllComponent } from './test-all/test-all.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 // Firebase credentials (okay to be public)
 const config = {
@@ -23,13 +26,14 @@ const config = {
 };
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent, VariablesPipe],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, VariablesPipe, TestAllComponent],
   imports: [BrowserModule,
     // AngularFire
     AngularFireModule.initializeApp(config),
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
-    AngularFireStorageModule // storage
+    AngularFireStorageModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
