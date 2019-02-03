99fcce73a2c118e22ef589d18b0023bae16c251e
Neal Tibrewala <Neal@Tibrewala.net>
2019-02-03 14:29:16 -0600

    Added testAll component

---
+++ src/app/test-all/test-all.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-all',
+  template: `
+    <p>
+      test-all works!
+    </p>
+  `,
+  styles: []
+})
+export class TestAllComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
