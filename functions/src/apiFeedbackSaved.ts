a655e756838b5ed0fc4d6129acd8edb5dfe09b19
Neal Tibrewala <Neal@Tibrewala.net>
2019-03-02 14:32:21 -0600

    adding feedback trigegr

--- functions/src/apiFeedbackSaved.ts
+++ functions/src/apiFeedbackSaved.ts
@@ -1,7 +1,23 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+
+const fs = admin.firestore();
 
 export const apiFeedbackSaved = functions.firestore
   .document("users/{userId}/playtestFeedback/{puzzleId}")
   .onWrite((snapshot, context) => {
     const after = snapshot.after.data();
+    const userId = context.params.userId;
+    const puzzleId = context.params.puzzleId;
+
+    // write feedback to puzzles collection
+    const doc = fs.doc(`puzzles/${puzzleId}/playtestFeedback/${userId}`);
+    if (!doc)
+      return Promise.reject("Unable to create reference to puzzle feedback doc");
+
+    const { puzzleRef, ...feedback } = after;
+
+    const lastChanged = admin.firestore.Timestamp.now();
+
+    return doc.set({ lastChanged, ...feedback }, { merge: true });
   });
