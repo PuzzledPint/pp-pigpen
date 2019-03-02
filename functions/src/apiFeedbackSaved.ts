04537aa2d10563f3717ee5e0a929e26aad219038
Neal Tibrewala <Neal@Tibrewala.net>
2019-03-02 15:09:05 -0600

    added some sanity checks

--- functions/src/apiFeedbackSaved.ts
+++ functions/src/apiFeedbackSaved.ts
@@ -6,14 +6,16 @@ const fs = admin.firestore();
 export const apiFeedbackSaved = functions.firestore
   .document("users/{userId}/playtestFeedback/{puzzleId}")
   .onWrite((snapshot, context) => {
+    if (!snapshot || !snapshot.after) return Promise.reject("No snapshot data");
     const after = snapshot.after.data();
+    if (!after) return Promise.reject("No snapshot data");
+
     const userId = context.params.userId;
     const puzzleId = context.params.puzzleId;
 
     // write feedback to puzzles collection
     const doc = fs.doc(`puzzles/${puzzleId}/playtestFeedback/${userId}`);
-    if (!doc)
-      return Promise.reject("Unable to create reference to puzzle feedback doc");
+    if (!doc) return Promise.reject("Unable to create reference to puzzle feedback doc");
 
     const { puzzleRef, ...feedback } = after;
 
