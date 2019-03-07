6d9606129cd6cccbafa2b540e288850af9491450
Neal Tibrewala <Neal@Tibrewala.net>
2019-03-07 01:34:34 -0600

    Changed to only webhook

--- functions/src/apiTravisDeploy.ts
+++ functions/src/apiTravisDeploy.ts
@@ -5,8 +5,10 @@ const fs = admin.firestore();
 
 export const apiTravisDeploy = functions.https.onRequest((request, response) => {
   // See https://docs.travis-ci.com/user/notifications/#configuring-webhook-notifications for request body (POST)
+  const build = request.query.build ? request.query.build : -1;
+  const commit = request.query.commit ? request.query.commit : 'XXXXXX';
 
-  return fs.doc('/settings/build').set(functions.config().travis.build).then(a => response.sendStatus(200)).catch(a => response.sendStatus(500));
+  return fs.doc('/settings/travis').set({build, commit}).then(a => response.sendStatus(200)).catch(a => response.sendStatus(500));
 });
 
 
