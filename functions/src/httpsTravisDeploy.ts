f5e6f4ca811228fd80ffee235ce584b5cc7469ea
Neal Tibrewala <Neal@Tibrewala.net>
2019-04-03 04:35:07 -0500

    emphasis on 'process'

--- functions/src/httpsTravisDeploy.ts
+++ functions/src/httpsTravisDeploy.ts
@@ -19,31 +19,27 @@ Bm9QMdieZMNry5s6aiMA9aSjDlNyedvSENYo18F+NYg1J0C0JiPYTxheCb4optr1
 
 export const httpsTravisDeploy = functions.https.onRequest((request, response) => {
   // See https://docs.travis-ci.com/user/notifications/#configuring-webhook-notifications for request body (POST)
-  console.log("apiTravisDeploy");
 
   if (!request) return response.status(500).send("No valid request found");
   console.log("request valid");
 
-  console.log("travisSignature = " + request.headers.signature);
-  const travisSignature = Buffer.from(request.headers.signature as string, 'base64');
-  console.log("travisSignatureBuffer = " + travisSignature);
+  const travisSignature = request.headers.signature as string;
   if (!travisSignature) return response.status(500).send("No authorization signature header found.");
+  console.log("Travis Signature found");
 
   const body = request.body;
   if (!body) return response.status(500).send("No valid body found");
   console.log("body JS  = " + JSON.stringify(body));
 
   const payload = JSON.parse(body.payload);
+  console.log("payload JS = " + payload);
   if (!payload) return response.status(500).send("No valid payload found");
-   console.log("payload JS = " + payload);
-   console.log("payload raw = "+ body.payload);
-  const payloadDecode = decodeURI(body.payload);
-  console.log("payload DU = " + payloadDecode);
 
   let verifier = crypto.createVerify('sha1');
-  verifier = verifier.update(payloadDecode);
-  const status = verifier.verify(travisPublicKey, travisSignature);
+  verifier = verifier.update(body.payload);
+  const status = verifier.verify(travisPublicKey, travisSignature, 'base64');
   if (!status) {
+    console.log("status = " + status);
     return response.status(500).send("Signature verification failed.");
   }
 
